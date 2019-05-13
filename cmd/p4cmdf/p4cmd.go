955129d5c2dc6b010356f6bbdc8e8405dc720772
Robert Cowham <rcowham@perforce.com>
2019-05-13 13:51:33 +0100

    Redo fix

--- cmd/p4cmdf/p4cmd.go
+++ cmd/p4cmdf/p4cmd.go
@@ -9,34 +9,15 @@ import (
 
 func main() {
 	p4 := p4.NewP4("", "")
-	fmt.Printf("args: %v", os.Args[1:])
 	if result, err := p4.RunP(os.Args[1:]); err != nil {
 		fmt.Printf("Error: %v %v", err, result)
 	} else {
-		for _, v := range result {
-			fmt.Printf("map map\n")
-			for k, j := range v {
-				fmt.Printf("%v: %v\n", k, j)
+		for _, r := range result {
+			for k, v := range r {
+				fmt.Printf("%v: %v\n", k, v)
 			}
-			// switch r := v.(type) {
-			// default:
-			// 	fmt.Printf("unexpected type %T\n", r) // %T prints whatever type t has
-			// case map[interface{}]interface{}:
-			// 	fmt.Printf("map map\n")
-			// 	for k, v := range r {
-			// 		fmt.Printf("%v: %v\n", k, v)
-			// 	}
-			// case bool:
-			// 	fmt.Printf("boolean %t\n", r) // t has type bool
-			// case int:
-			// 	fmt.Printf("integer %d\n", r) // t has type int
-			// case *bool:
-			// 	fmt.Printf("pointer to boolean %t\n", *r) // t has type *bool
-			// case *int:
-			// 	fmt.Printf("pointer to integer %d\n", *r) // t has type *int
-			// }
-			fmt.Printf("Results: %v", v)
+			fmt.Printf("\n")
 		}
-
+		fmt.Printf("\nResult: %v\n", result)
 	}
 }
